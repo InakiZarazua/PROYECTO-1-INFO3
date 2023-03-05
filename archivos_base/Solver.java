8f9921d35aa3bbc64ce9267dac4da24d0fc4af6f
InakiZarazua <109751542+InakiZarazua@users.noreply.github.com>
2023-03-05 02:16:10 +0000

    trabajo honesto jaja

--- archivos_base/Solver.java
+++ archivos_base/Solver.java
@@ -1,3 +1,5 @@
+import java.util.List;
+import java.util.ArrayList;
 /*
     Esta es su clase principal. El unico metodo que debe implementar es
     public String[] solve(Maze maze)
@@ -7,11 +9,63 @@ public class Solver{
 
     public Solver(){
         //Sientase libre de implementar el contructor de la forma que usted lo desee
+        
     }
+    public void moverse(Maze maze, List<String> soluciones, String way){
+       // if (maze.isExitSpace(int xIndex, int yIndex, int zIndex) == true){
+           // soluciones.add("["+ way +"]");
+          return;
+        
+    }
+/*
+    int MNorte = maze.moveNorth;
+    int MSur = maze.moveSouth;
+    int MOeste = maze.moveWest;
+    int MEste = maze.moveSouth;
+    int MArriba = maze.moveUp;
+    int MAbajo = moveDown;
+ */   
+
+    public String Norte(){
+        
+        String North = "N";
+        return North;
+    }
+     public String Sur(){
+        
+        String South = "S";
+        return South;
+    }
+     public String Oeste(){
+        
+        String West = "O";
+        return West;
+    }
+     public String Este(){
+        
+        String East = "E";
+        return East;
+    }
+     public String Arriba(){
+        
+        String Up = "U";
+        return Up;
+    }
+     public String Abajo(){
+        
+        String Down = "D";
+        return Down;
+    }
+    
+
 
     public String solve(Maze maze){
         //Implemente su metodo aqui. Sientase libre de implementar métodos adicionales
-        return "[-1]";
+        List<String> soluciones = new ArrayList<>();
+        //String[] Arraysoluciones = soluciones.stream().toArray
+
+        
+        return Norte();
     }
 
 }
